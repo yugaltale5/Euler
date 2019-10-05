08da9fe2166aa9932935a6efdb8a05f03f3a801c
Yugal Tale <yugaltale5@gmail.com>
2019-10-05 20:06:24 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,20 @@
+def evenFibSum(limit) : 
+    if (limit < 2) : 
+        return 0
+    ef1 = 0
+    ef2 = 2
+    sm= ef1 + ef2  
+    while (ef2 <= limit) :  
+        ef3 = 4 * ef2 + ef1 
+  
+        # If we go beyond limit, we break loop 
+        if (ef3 > limit) : 
+            break
+        ef1 = ef2 
+        ef2 = ef3 
+        sm = sm + ef2 
+      
+    return sm 
+  
+limit = 4000000
+print evenFibSum(limit)
