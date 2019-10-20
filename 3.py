88ed2f81e73be7e9234328d2205074d6fc405dfb
akashbadki007 <55804895+akashbadki007@users.noreply.github.com>
2019-10-20 22:18:17 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,23 +1,17 @@
  
 import math   
  
-def maxPrimeFactors (n): 
-       
-    maxPrime = -1
-      
-    
+def maxPrimeFactors (n):        
+    maxPrime = -1     
     while n % 2 == 0: 
         maxPrime = 2
-        n >>= 1     
-           
+        n >>= 1                
     for i in range(3, int(math.sqrt(n)) + 1, 2): 
         while n % i == 0: 
             maxPrime = i 
             n = n / i  
     if n > 2: 
-        maxPrime = n 
-      
-    return int(maxPrime) 
-  
+        maxPrime = n     
+    return int(maxPrime)   
 n = 600851475143
 print(maxPrimeFactors(n)) 
