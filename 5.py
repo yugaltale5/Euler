ae4a9b423bde02c3a9b539124f592a784a1c73ec
Nilesh Halge <nileshhalge01@gmail.com>
2021-10-20 23:05:28 -0700

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,3 +1,16 @@
 for i in range(0,5):
     print("*"*i)
 #Simple Star Pattern Program
+
+
+#problem 24
+import itertools
+
+def compute():
+	arr = list(range(10))
+	temp = itertools.islice(itertools.permutations(arr), 999999, None)
+	return "".join(str(x) for x in next(temp))
+
+
+if __name__ == "__main__":
+	print(compute())
