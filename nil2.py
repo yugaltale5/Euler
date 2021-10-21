c13e57d1c0d35d3bc4789714eb6d17e335ed237d
Nilesh Halge <nileshhalge01@gmail.com>
2021-10-20 23:33:58 -0700

    Update nil2.py

--- nil2.py
+++ nil2.py
@@ -1,3 +1,17 @@
+#problem10
+import eulerlib
+
+
+# Call the sieve of Eratosthenes and sum the primes found.
+def compute():
+	ans = sum(eulerlib.list_primes(1999999))
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(compute())
+	
+	
 #Problem 85
 import eulerlib
 
